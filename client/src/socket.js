c20fc831b05cb88d07e18605344785ce79384e9f
Marcos21c <marcosecondori@gmail.com>
2026-01-11 02:43:11 -0300

    config.

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // For MVP, assuming localhost:3001. In prod, this would be dynamic.
-const URL = "http://localhost:3001";
+const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
 
 export const socket = io(URL, {
     autoConnect: false
